1f81aef3aa564e470e2cf5ac9949493f0577f393
Hans <357274188@qq.com>
2018-08-10 18:02:30 +0800

    新增express+mysql

---
+++ server/sqlMap.js
@@ -0,0 +1,13 @@
+// sql语句
+var sqlMap = {
+    // 用户
+    user: {
+        add: "insert into user(username, Password) values (?, ?)",
+        select: "select * from user",
+        // select: "select * from [user] where 1=1 and  [where = ?]",
+        del: 'delete from user where username = ?',
+        update: 'update user set column = ? where column = ?'
+    }
+}
+
+module.exports = sqlMap;
